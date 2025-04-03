529eeb3a612f8299504df53c3bbe2e308f41eb41
oluwaseun-odueso <seunoduez@gmail.com>
2025-04-03 16:05:43 +0100

    chore: modified extended express type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,9 +3,7 @@ import { Request } from "express";
 declare global {
   namespace Express {
     interface Request {
-      staff?;
-      admin?;
-      superAdmin;
+      user?: any;
     }
   }
 }
